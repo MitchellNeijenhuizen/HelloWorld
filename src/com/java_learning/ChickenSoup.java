c5f84b893d477a52a6d39a6226a723d2ed29ed21
MitchellNeijenhuizen <m.a.j.w.neijenhuizen@gmail.com>
2020-04-03 15:16:30 +0200

    test

--- src/com/java_learning/ChickenSoup.java
+++ src/com/java_learning/ChickenSoup.java
@@ -2,7 +2,7 @@ package com.java_learning;
 
 public class ChickenSoup extends Food {
     public void taste() {
-        System.out.println("I am salty"); /////
+        System.out.println("I am salty!"); /////
     }
 }
 
